ebfc5afb0160a0568d198a0125d08be20166fb2c
Jerrin Eldo Mazhuvancherry P <jerin.jollybhavan@gmail.com>
2022-02-05 17:31:31 -0500

    Q4 final commit

--- src/js/main.js
+++ src/js/main.js
@@ -5,12 +5,13 @@ var reportsWidget = {
             '{{#.}}' +
                 '<article class="reports_item">' +
                     '<a href="{{cover}}" target="_blank">' +
-                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'
+                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'+
                     '</a>' +
                     '<footer class="reports_docs">' +
                         '{{#documents}}' +
                             '<h3 class="reports_title">' +
                                 '<a href="{{url}}" target="_blank">{{title}}</a>' +
+                                '<span class="file_info"> ({{file_size}} {{file_type}})</span>'+
                             '</h3>' +
                         '{{/documents}}' +
                     '</footer>' +
